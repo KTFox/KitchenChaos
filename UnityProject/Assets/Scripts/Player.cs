545a2082e053639b0f1a1ed32d65d93b5e611808
Trần Xuân Khôi <khoitran7122002@gmail.com>
2023-09-04 19:47:26 +0700

    Character Rotation.

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 5f;
 
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
@@ -20,10 +22,12 @@ public class Player : MonoBehaviour {
             inputVector.x = 1;
         }
 
-        inputVector = inputVector.normalized; //Why?
+        inputVector = inputVector.normalized;
 
-        Vector3 vectorDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += vectorDir * moveSpeed * Time.deltaTime;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
 
 }
