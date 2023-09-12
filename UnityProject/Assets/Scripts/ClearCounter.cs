38912280f5c3ba0f15d284eeb264006b3d2b25fd
Trần Xuân Khôi <khoitran7122002@gmail.com>
2023-09-12 17:12:31 +0700

    Player Pickup/Drop Object.

--- UnityProject/Assets/Scripts/ClearCounter.cs
+++ UnityProject/Assets/Scripts/ClearCounter.cs
@@ -5,7 +5,28 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
+        if (!HasKitchenObject()) {
+            // The counter doesn't have a kitchen object on it
 
+            if (player.HasKitchenObject()) {
+                // Player is carrying kitchen object
+
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                // Player isn't carrying a kitchen object
+            }
+        } else {
+            // The counter has a kitchen object on it
+
+            if (player.HasKitchenObject()) {
+                // Player is carrying a kitchen object
+            } else {
+                // Player isn't carrying a kitchen object
+
+                GetKitchenObject().SetKitchenObjectParent(player);
+                
+            }
+        }
     }
 
 }
--- UnityProject/Assets/Scripts/ContainerCounter.cs
+++ UnityProject/Assets/Scripts/ContainerCounter.cs
@@ -8,9 +8,13 @@ public class ContainerCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        OnPlayerGrappedObject?.Invoke(this, EventArgs.Empty);
+        if (!player.HasKitchenObject()) {
+            //Player isn't carrying a kitchen object
+
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrappedObject?.Invoke(this, EventArgs.Empty);
+        }
     }
 
 }
