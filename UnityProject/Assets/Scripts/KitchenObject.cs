e5e44d8da0f3417dd9559af0771885c71f890ea5
Trần Xuân Khôi <khoitran7122002@gmail.com>
2023-09-12 13:45:00 +0700

    Kitchen Object Parent.

--- UnityProject/Assets/Scripts/KitchenObject.cs
+++ UnityProject/Assets/Scripts/KitchenObject.cs
@@ -4,8 +4,31 @@ public class KitchenObject : MonoBehaviour {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public ClearCounter GetClearCounter() {
+        return clearCounter;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter) {
+        if (this.clearCounter != null) {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (this.clearCounter.HasKitchenObject()) {
+            Debug.LogError("The counter already has a kitchen object!!!");
+        }
+
+        this.clearCounter.SetKitchenObjet(this);
+
+        transform.parent = clearCounter.GetTheKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
     }
 
 }
