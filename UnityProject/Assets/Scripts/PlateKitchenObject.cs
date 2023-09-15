d9a04317175e2383fae854664c708086da1bc8ed
Trần Xuân Khôi <khoitran7122002@gmail.com>
2023-09-15 22:35:43 +0700

    Plate world UI Icon.

--- UnityProject/Assets/Scripts/PlateKitchenObject.cs
+++ UnityProject/Assets/Scripts/PlateKitchenObject.cs
@@ -36,5 +36,9 @@ public class PlateKitchenObject : KitchenObject {
         }
     }
 
+    public List<KitchenObjectSO> GetKitchenObjectSOList() {
+        return kitchenObjectSOList;
+    }
+
 }
 
